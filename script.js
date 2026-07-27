5643dcb554289d444309a91d49d38d6c3369972e
pupinupi <cool.lg-lg@yandex.ru>
2026-07-27 22:13:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function movePlayer(pos){
     return;
   }
 
-  token.style.left = (cell.x - 10) + "px";
+  token.style.left = (cell.x - 15) + "px";
 token.style.top = (cell.y - 25) + "px";
 }
 
