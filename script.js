8507df7db2da024525742f99c7af6a93311b5c83
pupinupi <cool.lg-lg@yandex.ru>
2026-07-27 23:11:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ function movePlayer(pos){
     (cell.x - 30) + "px";
 
   cellGlow.style.top =
-    (cell.y - 30) + "px";
+    (cell.y - 38) + "px";
 
   token.style.left =
     (cell.x - 15) + "px";
